1e26dbc013471336031b661d842874226e6596a7
Yaroslav Atamanets <yarik.atamanets@gmail.com>
2022-02-23 08:55:45 +0200

    HW9. Created a function for summing input numbers

---
+++ Homework 9/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function calcSum() { 
+ 
+    let result = [];
+    return function (a) {
+        result.push(a);
+        return result.reduce(function(acc, num) {
+            return acc + num;
+        }); 
+    };
+}
+  
+  let sum = calcSum();
+  console.log(sum(3));
+  console.log(sum(5));
+  console.log(sum(20));
+
